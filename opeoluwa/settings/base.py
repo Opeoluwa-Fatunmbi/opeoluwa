9bf06ce6a420feb331a6fcdbdfb9343ee38eb316
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-08 08:55:37 +0100

    [Add] Railway

--- opeoluwa/settings/base.py
+++ opeoluwa/settings/base.py
@@ -20,7 +20,8 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
+# ALLOWED_HOSTS = [".vercel.app", ".now.sh", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
