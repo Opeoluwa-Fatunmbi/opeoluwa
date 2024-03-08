1a5feba4738c8764a2469440ea4b42ba601b91ad
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2024-03-08 10:17:01 +0100

    [Fix] Log

--- opeoluwa/settings/base.py
+++ opeoluwa/settings/base.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import os
 from decouple import config
-import logging
+
+# import logging
 import logging.config
-from django.utils.log import DEFAULT_LOGGING
+
+# from django.utils.log import DEFAULT_LOGGING
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -185,72 +187,6 @@ EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 EMAIL_OTP_EXPIRE_SECONDS = config("EMAIL_OTP_EXPIRE_SECONDS")
-
-
-# Logging Settings
-logger = logging.getLogger(__name__)
-
-LOG_LEVEL = "INFO"
-
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "formatters": {
-            "console": {
-                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
-            },
-            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "formatter": "console",
-            },
-            "file": {
-                "level": "INFO",
-                "class": "logging.FileHandler",
-                "formatter": "file",
-                "filename": "logs/opeoluwa.log",
-            },
-            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-        },
-        "loggers": {
-            "": {
-                "level": "INFO",
-                "handlers": ["console", "file"],
-                "propagate": False,
-            },
-            "apps": {
-                "level": "INFO",
-                "handlers": ["console"],
-                "propagate": False,
-            },
-            "accounts": {
-                "level": "INFO",
-                "handlers": ["console"],
-                "propagate": False,
-            },
-            "common": {
-                "level": "INFO",
-                "handlers": ["console"],
-                "propagate": False,
-            },
-            "general": {
-                "level": "INFO",
-                "handlers": ["console"],
-                "propagate": False,
-            },
-            "portfolio": {
-                "level": "INFO",
-                "handlers": ["console"],
-                "propagate": False,
-            },
-            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
-        },
-    }
-)
 
 
 INTERNAL_IPS = [
